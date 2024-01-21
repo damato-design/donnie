a615babcfa88de8aa311492498ed988f3a2c3aad
Donnie D'Amato <fauxserious@gmail.com>
2024-01-21 16:10:19 -0500

    Astro update

--- src/components/utc.js
+++
@@ -1,8 +0,0 @@
-export default function utc(source) {
-  const d = source ? new Date(Date.parse(source)) : new Date();
-  const str = [d.getFullYear(), d.getMonth() + 1, d.getDate()].map((n) => {
-      const s = String(n);
-      return s.length === 1 ? s.padStart(2, '0') : s;
-  }).join('-');
-  return `${str}T00:00:00.000Z`;
-}
