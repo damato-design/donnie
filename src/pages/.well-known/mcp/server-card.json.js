83373216a4ae938e7c24a99e73e45f3572116d85
Donnie D'Amato <fauxserious@gmail.com>
2025-11-20 22:22:44 -0500

    Fix server card

--- src/pages/.well-known/mcp/server-card.json.js
+++ src/pages/.well-known/mcp/server-card.json.js
@@ -1,16 +1,19 @@
-export const GET = async () => {
+export const GET = async (req) => {
+    const name = "donnie-damato-mcp";
+    const uri = new URL("/.well-known/mcp/resources.json", req.url.origin).toString();
+
     const response = {
         $schema: "https://static.modelcontextprotocol.io/schemas/mcp-server-card/v1.json",
         version: "1.0",
         protocolVersion: "2025-06-18",
         serverInfo: {
-            name: "donnie-damato-mcp",
+            name,
             version: "1.0.0",
         },
         resources: [
             {
-                name: "resources",
-                uri: "/.well-known/mcp/resources.json",
+                name: `${name}-resources`,
+                uri,
                 mimeType: "application/json"
             }
         ]
