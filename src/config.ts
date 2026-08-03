f78f9408e884dffd312c04d185ea208a44e7bfc3
Donnie D'Amato <fauxserious@gmail.com>
2026-08-03 10:26:11 -0400

    Fix description length

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const siteConfig = {
    *
    * Default meta description for SEO and social sharing.
    */
-  description: 'User Experience Architect based in New York, author of Mise en Mode, and international speaker who bridges design and engineering.',
+  description: 'User Experience Architect in New York, author of Mise en Mode, and international speaker who bridges design and engineering.',
 
   /**
    * Author information
